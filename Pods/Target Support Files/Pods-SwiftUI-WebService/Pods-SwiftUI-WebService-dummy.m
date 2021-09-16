d20f1646b0f74d99397d4552682b6863803b023b
ceren <ceren@rockgames.rocks>
2021-09-16 15:39:55 +0300

    Session

---
+++ Pods/Target Support Files/Pods-SwiftUI-WebService/Pods-SwiftUI-WebService-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftUI_WebService : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftUI_WebService
+@end
---
+++ SwiftUI-WebService/ServiceController.swift
@@ -0,0 +1,15 @@
+//
+//  ServiceController.swift
+//  SwiftUI-WebService
+//
+//  Created by Ceren Taşsın on 13.08.2021.
+//  Copyright © 2021 ceren. All rights reserved.
+//
+
+import Foundation
+
+class ServiceController {
+    
+    
+    
+}
